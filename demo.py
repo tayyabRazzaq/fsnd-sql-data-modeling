b045c2d889425cc637163fc122eb25c32293db29
Tayyab Razzaq <tayyab.razzaq401@gmail.com>
2019-10-29 10:58:34 +0500

    feat: providing the values separately

--- demo.py
+++ demo.py
@@ -4,16 +4,20 @@ connection = psycopg2.connect('dbname=example user=postgres password=postgres')
 
 cursor = connection.cursor()
 
-cursor.execute(
-    '''
-    CREATE TABLE table2 (
-    id INTEGER PRIMARY KEY,
-    completed BOOLEAN NOT NULL DEFAULT False
-    );
-    '''
-)
+# cursor.execute(
+#     '''
+#     CREATE TABLE table2 (
+#     id INTEGER PRIMARY KEY,
+#     completed BOOLEAN NOT NULL DEFAULT False
+#     );
+#     '''
+# )
 
-cursor.execute('INSERT INTO table2 (id, completed) VALUES (1, true);')
+# cursor.execute('INSERT INTO table2 (id, completed) VALUES (1, true);')
+
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (2, False))
+
+
 
 connection.commit()
 
