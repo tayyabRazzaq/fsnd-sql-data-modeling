dafeb2fa7faa46b89c614cdc191426e4b2a014da
Tayyab Razzaq <tayyab.razzaq401@gmail.com>
2019-10-29 10:59:59 +0500

    feat: complete refactoring using string composition

--- demo.py
+++ demo.py
@@ -17,11 +17,15 @@ cursor = connection.cursor()
 
 # cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (2, False))
 
-cursor.execute('INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);',
-               {
-                   'id': 3,
-                   'completed': True
-               })
+
+data = {
+    'id': 4,
+    'completed': False
+}
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+cursor.execute(SQL, data)
 
 connection.commit()
 
