528c6cdaf421f9f29b8769b4e8c26741c288e77d
Tayyab Razzaq <tayyab.razzaq401@gmail.com>
2019-10-30 07:16:25 +0500

    feat: basic routes added

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, escape, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
